e505891b287f2fbf5710ee4fe5ed293bfa0d8f0a
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-08-03 20:51:43 -0300

    fixed Declaration and Identifier

--- src/main/java/model/Identifier.java
+++ src/main/java/model/Identifier.java
@@ -2,12 +2,18 @@ package model;
 
 public class Identifier implements AstComponent {
     private final String name;
+    private final IdentifierType type;
 
-    public Identifier(String name) {
+    public Identifier(String name, IdentifierType type) {
         this.name = name;
+        this.type = type;
     }
 
     public String getName() {
         return name;
+    }
+
+    public IdentifierType getType() {
+        return type;
     }
 }
