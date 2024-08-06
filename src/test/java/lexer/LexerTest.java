bb1d0ea775b7cf70b641789aa0f89402a50c97d4
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-08-06 16:45:06 -0300

    more tests

--- src/test/java/lexer/LexerTest.java
+++ src/test/java/lexer/LexerTest.java
@@ -82,7 +82,7 @@ class LexerTest {
 
     @Test
     void multiLineAssignation() {
-        String input = "let name: string = \"Joe\"; let lastName: string = \"Doe\";";
+        String input = "let name: string = \"Joe\"; \n let lastName: string = \"Doe\";";
         var tokens = lexer.lex(input);
         System.out.println(tokens);
         assertEquals(14, tokens.size());
@@ -102,6 +102,45 @@ class LexerTest {
         assertEquals(BaseTokenTypes.SEMICOLON, tokens.get(13).getType());
     }
 
+    @Test
+    void printlnStringTest() {
+        String input = "println(\"Hello, World!\");";
+        var tokens = lexer.lex(input);
+        System.out.println(tokens);
+        assertEquals(3, tokens.size());
+        assertEquals(BaseTokenTypes.PRINTLN, tokens.get(0).getType());
+        assertEquals(BaseTokenTypes.LITERAL, tokens.get(1).getType());
+        assertEquals(BaseTokenTypes.SEMICOLON, tokens.get(2).getType());
+    }
+
+    @Test
+    void printlnVariableTest() {
+        String input = "println(x);";
+        var tokens = lexer.lex(input);
+        System.out.println(tokens);
+        assertEquals(3, tokens.size());
+        assertEquals(BaseTokenTypes.PRINTLN, tokens.get(0).getType());
+        assertEquals(BaseTokenTypes.IDENTIFIER, tokens.get(1).getType());
+        assertEquals(BaseTokenTypes.SEMICOLON, tokens.get(2).getType());
+    }
+
+    @Test
+    void printCreatedVariable() {
+        String input = "let my_cool_variable: string = \"ciclon\";\nprintln(my_cool_variable);";
+        var tokens = lexer.lex(input);
+        System.out.println(tokens);
+        assertEquals(10, tokens.size());
+        assertEquals(BaseTokenTypes.LET, tokens.get(0).getType());
+        assertEquals(BaseTokenTypes.IDENTIFIER, tokens.get(1).getType());
+        assertEquals(BaseTokenTypes.COLON, tokens.get(2).getType());
+        assertEquals(BaseTokenTypes.TYPE, tokens.get(3).getType());
+        assertEquals(BaseTokenTypes.ASSIGNATION, tokens.get(4).getType());
+        assertEquals(BaseTokenTypes.LITERAL, tokens.get(5).getType());
+        assertEquals(BaseTokenTypes.SEMICOLON, tokens.get(6).getType());
+        assertEquals(BaseTokenTypes.PRINTLN, tokens.get(7).getType());
+        assertEquals(BaseTokenTypes.IDENTIFIER, tokens.get(8).getType());
+        assertEquals(BaseTokenTypes.SEMICOLON, tokens.get(9).getType());
+    }
     @Test
     void invalidIdentifiers() {
         String input = "let 1x: number = 5;";
