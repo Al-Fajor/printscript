ecbe32e1e7bc916750b7c5c038cc7fd519465c86
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-08-06 19:26:35 -0300

    detectors

--- src/main/java/lexer/factory/ErrorDetectorFactory.java
+++ src/main/java/lexer/factory/ErrorDetectorFactory.java
@@ -8,6 +8,9 @@ import java.util.List;
 
 public class ErrorDetectorFactory {
     public static List<LexicalErrorDetector> create() {
-        return List.of();
+        return List.of(
+                new UnfinishedSeparatorsDetector(),
+                new InvalidChainDetector()
+        );
     }
 }
