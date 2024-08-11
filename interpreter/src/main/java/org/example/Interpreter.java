d3119869ad633d60d6a2e31f115ce047efebbff1
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-08-10 21:07:39 -0300

    updated Interpreter interface

--- interpreter/src/main/java/org/example/Interpreter.java
+++ interpreter/src/main/java/org/example/Interpreter.java
@@ -1,9 +1,10 @@
 package org.example;
 
 import org.example.ast.AstComponent;
+import org.example.ast.statement.SentenceStatement;
 
 import java.util.List;
 
 public interface Interpreter {
-    void interpret(List<AstComponent> astList);
+    void interpret(List<SentenceStatement> astList);
 }
--- interpreter/src/main/java/org/example/PrintScriptInterpreter.java
+++ interpreter/src/main/java/org/example/PrintScriptInterpreter.java
@@ -1,27 +1,30 @@
 package org.example;
 
 import org.example.ast.AstComponent;
+import org.example.ast.statement.SentenceStatement;
+import org.example.ast.visitor.Visitor;
+import org.example.visitors.StatementVisitor;
 
 import java.util.List;
 
 public class PrintScriptInterpreter implements Interpreter {
     private final InterpreterState state;
-    private final StrategySelector strategySelector;
+    private final Visitor<Void> statementVisitor;
 
     public PrintScriptInterpreter() {
         state = new PrintScriptState();
-        strategySelector = new StrategySelector(state);
+		statementVisitor = new StatementVisitor(state);
     }
 
     public PrintScriptInterpreter(InterpreterState state) {
         this.state = state;
-        this.strategySelector = new StrategySelector(state);
+		statementVisitor = new StatementVisitor(state);
     }
 
     @Override
-    public void interpret(List<AstComponent> astList) {
-        for (AstComponent astComponent : astList) {
-            strategySelector.execute(astComponent);
+    public void interpret(List<SentenceStatement> astList) {
+        for (SentenceStatement statement : astList) {
+            statement.accept(statementVisitor);
         }
     }
 }
