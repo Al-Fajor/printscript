93e01fc06fb2f5e4ce13b1a7daea06c048c9b157
FranC-prog <franciscocalveyra24@gmail.com>
2024-08-11 17:38:16 -0300

    Added IfBuilder

---
+++ syntax/src/main/java/org/example/sentence/builder/IfBuilder.java
@@ -0,0 +1,13 @@
+package org.example.sentence.builder;
+
+import org.example.ast.AstComponent;
+import org.example.token.Token;
+
+import java.util.List;
+
+public class IfBuilder implements SentenceBuilder {
+  @Override
+  public AstComponent buildSentence(List<Token> tokens) {
+    return null;
+  }
+}
