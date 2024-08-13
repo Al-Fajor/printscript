d11e63c49939f9e1c9f01588e222c5857945ff33
FranC-prog <franciscocalveyra24@gmail.com>
2024-08-13 19:37:07 -0300

    Runner tests modifications

--- runner/src/main/java/org/example/Runner.java
+++ runner/src/main/java/org/example/Runner.java
@@ -1,8 +1,6 @@
 package org.example;
 
 import org.example.ast.*;
-import org.example.ast.statement.AssignationStatement;
-import org.example.ast.statement.FunctionCallStatement;
 import org.example.lexerresult.LexerFailure;
 import org.example.lexerresult.LexerResult;
 import org.example.result.SyntaxError;
@@ -10,7 +8,6 @@ import org.example.result.SyntaxResult;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 public class Runner {
@@ -23,22 +20,20 @@ public class Runner {
     LexerResult lexerResult = lexer.lex(code);
 
     if (!lexerResult.isSuccessful()) {
-      System.out.println(((LexerFailure) lexerResult).message());
-      return;
+      throw new RuntimeException(((LexerFailure) lexerResult).message());
     }
 
     SyntaxResult syntaxResult = syntaxAnalyzer.analyze(lexerResult.getTokens());
 
-    if(syntaxResult.isFailure() && syntaxResult instanceof SyntaxError){
-      System.out.println(((SyntaxError) syntaxResult).getReason());
-      return;
+    if(syntaxResult.isFailure()){
+      throw new RuntimeException(((SyntaxError) syntaxResult).getReason());
     }
 
     List<AstComponent> components = syntaxResult.getComponents();
     SemanticResult result = semanticAnalyzer.analyze(components);
 
-    if(result.isFailure()){
-      System.out.println("Semantic error");
+    if(!result.isSuccessful()){
+      throw new RuntimeException("Semantic error");
     }
     else{
       interpreter.interpret(components);
@@ -51,24 +46,15 @@ public class Runner {
 
   private static SemanticAnalyzer getSemanticAnalyzer() {
     SemanticAnalyzer semanticAnalyzer;
-
     {
-      Map<Class<? extends AstComponent>, Resolver> resolverMap = Map.of(
-        AssignationStatement.class, new AssignationResolver(),
-        BinaryExpression.class, new BinaryExpressionResolver(),
-        Literal.class, new LiteralResolver(),
-        Identifier.class, new IdentifierResolver(),
-        FunctionCallStatement.class, new FunctionCallResolver(),
-        Parameters.class, new ParametersResolver()
-      );
       MapEnvironment env = new MapEnvironment(
-        new HashMap<String, DeclarationType>(),
+        new HashMap<>(),
         Set.of(
           new Signature("println", List.of(DeclarationType.NUMBER)),
           new Signature("println", List.of(DeclarationType.STRING))
         )
       );
-      semanticAnalyzer = new AnalyzerImpl(resolverMap, env);
+      semanticAnalyzer = new SemanticAnalyzerImpl(env);
     }
     return semanticAnalyzer;
   }
