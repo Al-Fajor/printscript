e448e0686ce4c477d16c31fdbece13e180a80494
FranC-prog <franciscocalveyra24@gmail.com>
2024-08-13 22:55:45 -0300

    SyntaxAnalyzer huge refactor

---
+++ syntax/src/main/java/org/example/sentence/validator/ElseSentenceValidator.java
@@ -0,0 +1,12 @@
+package org.example.sentence.validator;
+
+import org.example.token.Token;
+
+import java.util.List;
+
+public class ElseSentenceValidator implements SentenceValidator {
+  @Override
+  public boolean isValidSentence(List<Token> tokens) {
+    return false;
+  }
+}
