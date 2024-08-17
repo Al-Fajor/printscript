1de227c4399ad1ab7fe80668e7bf781dc9631f5f
Tomás <troldanborjas@mail.austral.edu.ar>
2024-08-17 19:40:05 -0300

    Specify Observable implementation in SemanticAnalyzer

--- semantics/src/main/java/org/example/SemanticAnalyzer.java
+++ semantics/src/main/java/org/example/SemanticAnalyzer.java
@@ -3,6 +3,6 @@ package org.example;
 import java.util.List;
 import org.example.ast.AstComponent;
 
-public interface SemanticAnalyzer {
+public interface SemanticAnalyzer extends Observable<String> {
 	SemanticResult analyze(List<AstComponent> asts);
 }
