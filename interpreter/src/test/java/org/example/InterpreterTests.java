1cfae4b1b33d34e51585181ddb9a9a8849b41c01
FranC-prog <franciscocalveyra24@gmail.com>
2024-08-18 16:05:59 -0300

    InterpreterTests minor refactor

--- interpreter/src/test/java/org/example/InterpreterTests.java
+++ interpreter/src/test/java/org/example/InterpreterTests.java
@@ -1,35 +1,24 @@
 package org.example;
 
-import java.io.IOException;
-import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.util.stream.Stream;
 
-public class InterpreterTests {
+import org.example.test.TestBuilder;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.function.Executable;
+
+public class InterpreterTests extends TestBuilder {
 	private final InterpreterTester tester = new InterpreterTester();
+  private static final String TEST_CASES = "src/test/resources/test_cases";
 
-	@Test
-	public void testAssignation() throws IOException {
-		tester.test("src/test/resources/test_cases/assign_numeric_variable.json");
-		tester.test("src/test/resources/test_cases/assign_string_variable.json");
-	}
+  @TestFactory
+  protected Stream<DynamicTest> testAllDirectoryCases() {
+    return super.testAllDirectoryCases(TEST_CASES);
+  }
 
-	@Test
-	public void testDeclaration() throws IOException {
-		tester.test("src/test/resources/test_cases/declare_numeric_variable.json");
-		tester.test("src/test/resources/test_cases/declare_string_variable.json");
-	}
-
-	@Test
-	public void testNumberOperations() throws IOException {
-		tester.test("src/test/resources/test_cases/number_operations.json");
-	}
-
-	@Test
-	public void testPrint() throws IOException {
-		tester.test("src/test/resources/test_cases/print_variables.json");
-	}
-
-	@Test
-	public void testConcatenation() throws IOException {
-		tester.test("src/test/resources/test_cases/string_concatenation.json");
-	}
+  @Override
+  protected Executable getTestExecutable(File testFile) {
+    return () -> tester.test(testFile.getPath());
+  }
 }
