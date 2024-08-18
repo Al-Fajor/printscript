a3bb54c5b740d6c0fc2e3b725e7d5176f7fa29c7
FranC-prog <franciscocalveyra24@gmail.com>
2024-08-18 16:07:12 -0300

    InterpreterTests minor refactor

--- interpreter/src/test/java/org/example/InterpreterTests.java
+++ interpreter/src/test/java/org/example/InterpreterTests.java
@@ -2,7 +2,6 @@ package org.example;
 
 import java.io.File;
 import java.util.stream.Stream;
-
 import org.example.test.TestBuilder;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
@@ -10,15 +9,15 @@ import org.junit.jupiter.api.function.Executable;
 
 public class InterpreterTests extends TestBuilder {
 	private final InterpreterTester tester = new InterpreterTester();
-  private static final String TEST_CASES = "src/test/resources/test_cases";
+	private static final String TEST_CASES = "src/test/resources/test_cases";
 
-  @TestFactory
-  protected Stream<DynamicTest> testAllDirectoryCases() {
-    return super.testAllDirectoryCases(TEST_CASES);
-  }
+	@TestFactory
+	protected Stream<DynamicTest> testAllDirectoryCases() {
+		return super.testAllDirectoryCases(TEST_CASES);
+	}
 
-  @Override
-  protected Executable getTestExecutable(File testFile) {
-    return () -> tester.test(testFile.getPath());
-  }
+	@Override
+	protected Executable getTestExecutable(File testFile) {
+		return () -> tester.test(testFile.getPath());
+	}
 }
