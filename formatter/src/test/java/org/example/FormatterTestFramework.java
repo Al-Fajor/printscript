cb1e9778f3bb1325c99e4ed516069ef7b375645f
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-08-21 22:00:21 -0300

    test refactor

--- formatter/src/test/java/org/example/FormatterTestFramework.java
+++ formatter/src/test/java/org/example/FormatterTestFramework.java
@@ -1,6 +1,16 @@
 package org.example;
 
+import org.example.io.AstBuilder;
+import org.example.io.FileParser;
+
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -8,23 +18,33 @@ public class FormatterTestFramework {
 
     public void testRules(String path) throws IOException {
         AstBuilder astBuilder = new AstBuilder();
-        FileParser fileParser = new FileParser();
-        TestRuleMapFactory testRuleMapFactory = new TestRuleMapFactory(path + "/formatter.rules.json");
+        MapFromFile mapFromFile = new MapFromFile();
+        TestRuleMapFactory testRuleMapFactory = new TestRuleMapFactory(path + "/rules.json");
         Formatter formatter = new PrintScriptFormatter(testRuleMapFactory);
-//        TODO get all non rule files from directory instead of using this array
-        String[] cases = new String[]{
-                "variable_assignation",
-                "string_assignation",
-                "print_operation",
-                "variable_assignation2",
-                "string_concatenation",
-                "print_variables"
-        };
-        for (String testCase : cases) {
-            String jsonPath = path + "/" + testCase + ".json";
+        Map<String, String> codes = mapFromFile.getMapFromFile(path + "/codes.json");
+        List<Path> cases = getAllFiles("src/test/resources/asts");
+        for (Path testCase : cases) {
+            String jsonPath = testCase.toString();
             System.out.println(jsonPath);
             String code = formatter.format(astBuilder.buildFromJson(jsonPath));
-            assertEquals(fileParser.getCode(jsonPath), code);
+            assertEquals(codes.get(extractFileNameWithoutExtension(testCase)), code);
         }
+    }
+
+    private List<Path> getAllFiles(String directoryPath) throws IOException {
+        try (Stream<Path> paths = Files.walk(Paths.get(directoryPath))) {
+            return paths
+                    .filter(Files::isRegularFile)
+                    .collect(Collectors.toList());
+        }
+    }
+
+    private String extractFileNameWithoutExtension(Path path) {
+        String fileName = path.getFileName().toString();
+        int dotIndex = fileName.lastIndexOf('.');
+        if (dotIndex != -1) {
+            return fileName.substring(0, dotIndex);
+        }
+        return fileName;
     }
 }
