30a15f96e952d798463fa63c28cb91a14e6658ea
FranCalveyra <franciscocalveyra24@gmail.com>
2024-08-28 12:17:02 -0300

    Continued Syntax refactor

--- syntax/src/main/java/org/example/sentence/validator/SentenceValidator.java
+++ syntax/src/main/java/org/example/sentence/validator/SentenceValidator.java
@@ -3,40 +3,35 @@ package org.example.sentence.validator;
 import static org.example.token.BaseTokenTypes.*;
 
 import java.util.List;
+import java.util.Stack;
 import org.example.sentence.validator.validity.InvalidSentence;
 import org.example.sentence.validator.validity.ValidSentence;
 import org.example.sentence.validator.validity.Validity;
 import org.example.sentence.validator.validity.rule.*;
 import org.example.token.Token;
-import org.example.token.TokenType;
 
 public class SentenceValidator {
+	private final List<ValidityRule> rules;
+
+	public SentenceValidator(List<ValidityRule> rules) {
+		this.rules = rules;
+	}
+
 	private final String notAnError = "Not an error";
 
 	public Validity getSentenceValidity(List<Token> tokens) {
-		List<ValidityRule> rules = getSentenceRules(tokens.getFirst().getType());
 		if (rules == null)
 			return new InvalidSentence(
 					"Invalid sentence. Should begin with PRINTLN, FUNCTION, IDENTIFIER or DECLARATION");
-		TokenValidator validator = new TokenValidator();
 
 		for (int i = 0; i < tokens.size(); i++) {
 			Token token = tokens.get(i);
 			Token nextToken = i + 1 >= tokens.size() ? null : tokens.get(i + 1);
 
 			// Hardcoded SEPARATOR case, may need optimization
-
 			if (token.getType() == SEPARATOR) {
-				String validatorMessage = validator.getValidityMessage(token, nextToken);
-				if (!(validator.areParenthesesBalanced(tokens)
-						&& validatorMessage.equals(notAnError))) {
-					return new InvalidSentence(validatorMessage);
-				} else continue;
-			}
-
-			if (validator.isNotSpecialToken(token)) {
-				String message = validator.getValidityMessage(token, nextToken);
-				if (!message.equals(notAnError)) return new InvalidSentence(message);
+				if (!areParenthesesBalanced(tokens))
+					return new InvalidSentence("Unbalanced Parenthesis");
 			}
 
 			String ownValidityMessage = getOwnMessage(token, nextToken, rules);
@@ -46,32 +41,27 @@ public class SentenceValidator {
 		return new ValidSentence();
 	}
 
-	private List<ValidityRule> getSentenceRules(TokenType type) {
-		return switch (type) {
-			case PRINTLN, FUNCTION ->
-					List.of(
-							new NextTokenShouldExist(),
-							new FunctionCallRule(),
-							new LiteralRule(),
-							new IdentifierOnFunctionRule());
-			case LET ->
-					List.of(
-							new NextTokenShouldExist(),
-							new DeclarationRule(),
-							new IdentifierOnDeclarationRule(),
-							new ColonRule(),
-							new TypeRule(),
-							new LiteralRule(),
-							new AssignationOnDeclarationRule());
-			case IDENTIFIER -> List.of(new NextTokenShouldExist(), new ReassignationRule());
-			default -> null;
-		};
-	}
-
 	private String getOwnMessage(Token token, Token nextToken, List<ValidityRule> rules) {
 		for (ValidityRule rule : rules) {
 			if (!rule.isValidRule(token, nextToken)) return rule.getErrorMessage();
 		}
 		return notAnError;
+	}
+
+	private boolean areParenthesesBalanced(List<Token> tokens) {
+		Stack<String> stack = new Stack<>();
+		// TODO: when we add them, add the "{" keys case
+
+		for (Token token : tokens) {
+			if (token.getValue().equals("(")) {
+				stack.push(token.getValue());
+			} else if (token.getValue().equals(")")) {
+				if (stack.isEmpty() || !stack.pop().equals("(")) {
+					return false;
+				}
+			}
+		}
+
+		return stack.isEmpty();
 	}
 }
