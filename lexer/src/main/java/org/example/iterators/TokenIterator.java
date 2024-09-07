94048ced3c6e2c2cdec34a589555452fa6fc1258
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-09-07 14:18:12 -0300

    sacando la pata

--- lexer/src/main/java/org/example/iterators/TokenIterator.java
+++ lexer/src/main/java/org/example/iterators/TokenIterator.java
@@ -5,7 +5,6 @@ import java.util.Iterator;
 import java.util.Queue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import org.example.Pair;
 import org.example.factory.TokenPatternFactory;
 import org.example.factory.TokenRegex;
@@ -18,11 +17,11 @@ public class TokenIterator implements Iterator<Token> {
 	private final Pattern pattern;
 	private final String input;
 	private final Matcher matcher;
-    private final int lineNumber;
+	private final int lineNumber;
 
 	public TokenIterator(String input, int lineNumber) {
 		this.input = input;
-        this.lineNumber = lineNumber;
+		this.lineNumber = lineNumber;
 		this.tokenQueue = new ArrayDeque<>();
 		this.pattern = TokenPatternFactory.createPattern(TokenRegex.getRegexMap());
 		this.matcher = pattern.matcher(this.input);
@@ -44,18 +43,19 @@ public class TokenIterator implements Iterator<Token> {
 	private void addToken(Queue<Token> tokens, Matcher matcher) {
 		for (BaseTokenTypes baseTokenTypes : BaseTokenTypes.values()) {
 			if (matcher.group(baseTokenTypes.name()) != null) {
-                int currentLine = lineNumber + PositionServices.getLine(input, matcher.start());
-                System.out.println(new Token(
-                        baseTokenTypes,
-                        new Pair<>(currentLine, PositionServices.getPositionInLine(input, matcher.start()) + 1),
-                        new Pair<>(currentLine, PositionServices.getPositionInLine(input, matcher.end()) + 1),
-                        matcher.group(baseTokenTypes.name())));
+				int currentLine = lineNumber + PositionServices.getLine(input, matcher.start());
 				tokens.add(
 						new Token(
-                                baseTokenTypes,
-                                new Pair<>(currentLine, PositionServices.getPositionInLine(input, matcher.start()) + 1),
-                                new Pair<>(currentLine, PositionServices.getPositionInLine(input, matcher.end()) + 1),
-                                matcher.group(baseTokenTypes.name())));
+								baseTokenTypes,
+								new Pair<>(
+										currentLine,
+										PositionServices.getPositionInLine(input, matcher.start())
+												+ 1),
+								new Pair<>(
+										currentLine,
+										PositionServices.getPositionInLine(input, matcher.end())
+												+ 1),
+								matcher.group(baseTokenTypes.name())));
 			}
 		}
 	}
--- staticanalyzer/src/main/java/org/example/PrintScriptSca.java
+++ staticanalyzer/src/main/java/org/example/PrintScriptSca.java
@@ -1,13 +1,13 @@
 package org.example;
 
-import org.example.lexerresult.LexerSuccess;
-import org.example.token.Token;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import org.example.lexerresult.LexerSuccess;
+import org.example.token.BaseTokenTypes;
+import org.example.token.Token;
 
 public class PrintScriptSca implements StaticCodeAnalyzer {
 	private final Map<ConfigAttribute, String> configMap;
@@ -19,28 +19,47 @@ public class PrintScriptSca implements StaticCodeAnalyzer {
 	@Override
 	public List<Result> analyze(Iterator<String> input) {
 		List<Result> results = new ArrayList<>();
-        Lexer lexer = new PrintScriptLexer();
-        Result lexerResult = lexer.lex(input);
-        if (!lexerResult.isSuccessful()){
-            throw new RuntimeException("Lexer failed: " + lexerResult);
-        }
-        Iterator<Token> tokenIterator = ((LexerSuccess) lexerResult).getTokens();
-        for (ConfigAttribute entry : configMap.keySet()) {
-            switch (entry) {
-                case IDENTIFIER_FORMAT ->
-                        results.addAll(
-                                new IdentifierStrategy(configMap.get(entry)).analyze(tokenIterator));
-                case PRINTLN_EXPRESSIONS ->
-                        results.addAll(
-                                new PrintlnExpressionsStrategy(configMap.get(entry))
-                                        .analyze(tokenIterator));
-                default -> throw new IllegalStateException("Unexpected value: " + entry);
-            }
-        }
+		Lexer lexer = new PrintScriptLexer();
+		Result lexerResult = lexer.lex(input);
+		if (!lexerResult.isSuccessful()) {
+			throw new RuntimeException("Lexer failed: " + lexerResult);
+		}
+		Iterator<Token> tokenIterator = ((LexerSuccess) lexerResult).getTokens();
+		List<Token> tokenList = new ArrayList<>();
+		while (tokenIterator.hasNext()) {
+			Token token = tokenIterator.next();
+			tokenList.add(token);
+			if (isEndOfBlock(token)) {
+				analyzeTokens(tokenList, results);
+				tokenList.clear();
+			}
+		}
 
 		if (results.isEmpty()) {
 			return List.of(new SuccessResult());
 		}
 		return results;
+	}
+
+	private boolean isEndOfBlock(Token token) {
+		return token.getType().equals(BaseTokenTypes.SEMICOLON)
+				|| (token.getType().equals(BaseTokenTypes.SEPARATOR)
+						&& token.getValue().equals("}"));
+	}
+
+	private void analyzeTokens(List<Token> tokens, List<Result> results) {
+		for (ConfigAttribute entry : configMap.keySet()) {
+			switch (entry) {
+				case IDENTIFIER_FORMAT ->
+						results.addAll(
+								new IdentifierStrategy(configMap.get(entry))
+										.analyze(tokens.iterator()));
+				case PRINTLN_EXPRESSIONS ->
+						results.addAll(
+								new PrintlnExpressionsStrategy(configMap.get(entry))
+										.analyze(tokens.iterator()));
+				default -> throw new IllegalStateException("Unexpected value: " + entry);
+			}
+		}
 	}
 }
