9d95903ebd86a52e285ade9357dfd778dc9cfdf9
Tomás <troldanborjas@mail.austral.edu.ar>
2024-09-12 07:11:00 -0300

    Fixed tests

--- interpreter/src/main/java/org/example/observer/PrintBrokerObserver.java
+++ interpreter/src/main/java/org/example/observer/PrintBrokerObserver.java
@@ -1,11 +1,11 @@
 package org.example.observer;
 
 public class PrintBrokerObserver implements BrokerObserver<String> {
-	private StringBuilder printedOutput = new StringBuilder();
+	private final StringBuilder printedOutput = new StringBuilder();
 
 	@Override
 	public void update(String change) {
-		printedOutput.append(change);
+		printedOutput.append(change).append("\n");
 		System.out.println(change);
 	}
 
